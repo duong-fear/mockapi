eb39a819a310033ae0fade70076c04155a09ff43
Brian <73123331+du0ngpv@users.noreply.github.com>
2022-03-24 10:53:39 +0700

    add public dir

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import payload from "./payload.js";
 var app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+    res.send(payload[req.path]);
+});
 
 app.get("/", function (req, res) {
     res.send(payload[req.path]);
