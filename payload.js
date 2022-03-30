55d1be86d3d39b0e99fdeff4d5c4626b1451896c
brian <duong@fear.io>
2022-03-30 09:49:35 +0700

    upd

--- payload.js
+++ payload.js
@@ -15,6 +15,8 @@ const payload = {
                 splash_hash: "a85032fcba6a6392a35f5c2c87449730",
                 lastest: '1.2.15',
                 compatible: '1.2.0',
+                hidden: false,
+                maintenance: true,
             },
             {
                 id: 2,
@@ -23,6 +25,8 @@ const payload = {
                 splash_hash: "b24d816da24a4077b9c812c3cc46b275",
                 lastest: '3.2.7',
                 compatible: '3.0.0',
+                hidden: false,
+                maintenance: false,
             },
             {
                 id: 3,
@@ -31,6 +35,8 @@ const payload = {
                 splash_hash: "87b3d1c3c4e00a0b71d9963c89a74988",
                 lastest: '1.14.51',
                 compatible: '1.14.50',
+                hidden: false,
+                maintenance: false,
             },
             {
                 id: 4,
@@ -39,6 +45,8 @@ const payload = {
                 splash_hash: "9dbbe4d8988f868173f6ad9ea8b60a19",
                 lastest: '2.3.7',
                 compatible: '2.2.0',
+                hidden: false,
+                maintenance: false,
             },
             {
                 id: 5,
@@ -47,6 +55,8 @@ const payload = {
                 splash_hash: "91903e676cd3fdf6df6fcb9ecc0117dc",
                 lastest: '0.1.4',
                 compatible: '0.1.0',
+                hidden: false,
+                maintenance: false,
             },
         ]
     },
